b48f16ae1e5f1019b31e23f3cdaef0960abd3578
zuratsintsadze <zura.aug@gmail.com>
2019-07-13 17:28:14 +0400

    add filter js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
-$( "#burger-btn" ).click(function() {
+$(document).ready(function() {
+  $("#burger-btn").click(function() {
     event.preventDefault();
-    $( "div.buregrmenu" ).toggleClass('open');
-    $( 'html' ).toggleClass('overflow-h');
-});
+    $("div.buregrmenu").toggleClass("open");
+    $("html").toggleClass("overflow-h");
+  });
 
-$( ".search-drop-toggle" ).click(function() {
+  $(".search-drop-toggle").click(function() {
     event.preventDefault();
-    $( "section.search-section" ).toggleClass('open');
-    $( 'html' ).toggleClass('overflow-h');
+    $("section.search-section").toggleClass("open");
+    $("html").toggleClass("overflow-h");
+  });
+
+  // header animation
+  $(".animated-menu").hover(function() {
+    // console.log($(this).find(".underbar").css());
+  });
+
+  $(".product-filter-menu").click(function() {
+    if (
+      $(this)
+        .next(".submenu")
+        .css("display") == "block"
+    ) {
+      $(this)
+        .next(".submenu")
+        .fadeOut(300);
+    } else {
+      $("ul.submenu").fadeOut(300);
+      $(this)
+        .next(".submenu")
+        .fadeIn(300);
+    }
+  });
 });
